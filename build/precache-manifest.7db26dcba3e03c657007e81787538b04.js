4f1e2c8d22f0ea69677377c88bcf89332eda2b9b
Mark <SwiftTactics@gmail.com>
2020-08-28 23:54:51 -0400

    Demonym launch

---
+++ build/precache-manifest.7db26dcba3e03c657007e81787538b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3599e948cef23b8f80c2d7662a4e219d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1e53304209e98357468",
+    "url": "/static/css/main.3881221e.chunk.css"
+  },
+  {
+    "revision": "11bd570f9d0625617c4a",
+    "url": "/static/js/2.fb9aae29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb9aae29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e53304209e98357468",
+    "url": "/static/js/main.dff546cd.chunk.js"
+  },
+  {
+    "revision": "5bf92f78e369daf30b89",
+    "url": "/static/js/runtime-main.cae8fa93.js"
+  }
+]);
